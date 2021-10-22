1330e55aa223f04be641e6a148fe34fe0ac19d38
makennawold <makennawold@gmail.com>
2021-10-22 11:21:33 -0500

    added required fields, max lengths, added axios call, added reset to fields

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+const axios = require("axios");
+
 export default function SignupForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
-    //axios call here
+    // axios({
+    //   method: "post",
+    //   url: "",
+    //   data: { data },
+    // })
+    axios.post("http://localhost:5000", { data }).catch(function(error) {
+      console.log(error);
+    });
     console.log(data);
+    reset();
   };
 
   return (
     <form className="signupForm" onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} placeholder="first name" />
-      <input {...register("lastName")} placeholder="last name" />
-      <input {...register("email")} placeholder="email" />
+      <input
+        {...register("firstName", { required: true, maxLength: 20 })}
+        placeholder="first name"
+      />
+      <input
+        {...register("lastName", { required: true, maxLength: 20 })}
+        placeholder="last name"
+      />
+      <input
+        {...register("email", { required: true, maxLength: 320 })}
+        placeholder="email"
+      />
       <input type="submit" id="submit-button" />
     </form>
   );
